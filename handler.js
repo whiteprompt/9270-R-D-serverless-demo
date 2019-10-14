75af62145ce6df4afa5ffc5891f1ec7aec4c3186
Fabricio Matías Quagliariello <matias.quaglia@gmail.com>
2019-10-14 14:07:34 -0300

    initial load

---
+++ handler.js
@@ -0,0 +1,21 @@
+const { GraphQLServerLambda } = require("graphql-yoga");
+const fs = require("fs");
+
+const typeDefs = fs.readFileSync("./schema.gql").toString('utf-8');
+
+const resolvers = {
+    Query: {
+        aurora_getUser: require("./resolver/Query/aurora_getUser").func
+    },
+    Mutation: {
+        aurora_createUser: require("./resolver/Mutation/aurora_createUser").func
+    }
+};
+
+const lambda = new GraphQLServerLambda({
+    typeDefs,
+    resolvers
+});
+
+exports.server = lambda.graphqlHandler;
+exports.playground = lambda.playgroundHandler;
